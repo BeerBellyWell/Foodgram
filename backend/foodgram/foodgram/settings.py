10b56959116edcbbf668b72ca1e16e778945d7a9
Андрей Чернышев <candreey17@mail.ru>
2023-06-09 01:20:35 +1200

    v2.5

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -3,6 +3,10 @@ import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
