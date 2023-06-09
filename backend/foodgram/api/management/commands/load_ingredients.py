4927c2a8183f8476be9c58577601141c8ff5680a
Андрей Чернышев <candreey17@mail.ru>
2023-06-10 08:29:52 +1200

    v2.11

--- backend/foodgram/api/management/commands/load_ingredients.py
+++ backend/foodgram/api/management/commands/load_ingredients.py
@@ -2,10 +2,12 @@ import csv
 import os
 import psycopg2
 
-
+from dotenv import load_dotenv
 from django.core.management import BaseCommand
 
-from recipes.models import Ingredient
+
+load_dotenv()
+
 
 class Command(BaseCommand):
     help = 'Заполнить БД данными из csv файла'
@@ -21,12 +23,22 @@ class Command(BaseCommand):
 
         cursor = connect.cursor()
 
-        with open(
-            'foodgram/data/ingredients.scv',
-            'r', encoding='UTF-8') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                _, created = Ingredient.objects.create(
-                    name=row[0],
-                    measurement_unit=row[1],
-                )
+        with open('api/management/commands/data/ingredients.csv',
+                  'r', encoding='UTF-8') as file:
+            contents = csv.reader(file)
+            col_names, number_questions = self.get_col_names(cursor,
+                                                             'app_ingredient')
+            insert_records = (f'INSERT INTO app_ingredient ({col_names})'
+                              f'VALUES({number_questions})')
+            cursor.executemany(insert_records, contents)
+
+        connect.commit()
+        connect.close()
+
+    def get_col_names(self, cursor, tablename):
+        cursor.execute(f"SELECT * FROM {tablename}")
+        col_names = ', '.join([x[0] for x in cursor.description
+                               if x[0] != 'id'])
+        number_questions = ', '.join(['%s' for x in
+                                      range(len(cursor.description) - 1)])
+        return col_names, number_questions
